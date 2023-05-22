954a9ed2bda97ce21ec0970d7cd6fd1ffe998ede
Mariia Kuryleva <maru.kuryleva@gmail.com>
2023-05-22 21:37:31 +0200

    refactor: change main page

---
+++ src/js/modules/pages/main.js
@@ -0,0 +1,9 @@
+import { categories } from "../cards";
+import { mainContent } from "../globals";
+
+for (let key in categories) {
+    let col = document.createElement("div");
+    col.className = "col mb-5";
+    col.appendChild(categories[key].createNode());
+    mainContent.appendChild(col);
+}
